07716e36bf963bc48b9831f64a3a121aaf7a0bc9
Xiaoling Huang <linccccoln@gmail.com>
2022-10-02 18:34:18 -0700

    Added ejsMate and bootstrap boilerplate

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const Campground = require('./models/campground');
+const ejsMate = require("ejs-mate");
 const methodOverride = require('method-override');
-
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/campAdvisor");
 
@@ -16,6 +16,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
