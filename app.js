2adf63725a70bfe58ebadd43c007106fdfbe025d
Xiaoling Huang <linccccoln@gmail.com>
2022-10-06 18:52:29 -0700

    Added error template

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const Campground = require('./models/campground');
 const ejsMate = require("ejs-mate");
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const mongoose = require("mongoose");
+
 mongoose.connect("mongodb://localhost:27017/campAdvisor");
 
 const db = mongoose.connection;
@@ -16,9 +17,10 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsMate);
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
@@ -39,6 +41,7 @@ app.get("/campgrounds/new", (req, res) => {
 });
 
 app.post("/campgrounds", catchAsync(async (req, res, next) => {
+    if (!req.body.campground) throw new ExpressError("Invalid Data", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -68,6 +71,11 @@ app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
 }));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
