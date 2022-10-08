76806427d8718b4403bb5771e1aaf41d6050784a
Xiaoling Huang <linccccoln@gmail.com>
2022-10-08 11:06:30 -0700

    "Added review creation"

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { campgroundSchema } = require("./schemas.js");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const Review = require("./models/review");
 
 mongoose.connect("mongodb://localhost:27017/campAdvisor");
 
@@ -86,6 +87,18 @@ app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
 })
+
+
+
+app.post("/campgrounds/:id/reviews", catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
 
 
 app.use((err, req, res, next) => {
