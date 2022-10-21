af47066918f533fc60e2bd6e250de82e69e3dffe
Xiaoling Huang <linccccoln@gmail.com>
2022-10-21 22:02:28 +0900

    Added author to campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -38,6 +38,7 @@ router.get("/new", isLoggedIn, (req, res) => {
 
 router.post("/", isLoggedIn, validateCampgound, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash("success", "Successfully created a new campground!");
     res.redirect(`/campgrounds/${campground._id}`)
@@ -45,7 +46,7 @@ router.post("/", isLoggedIn, validateCampgound, catchAsync(async (req, res, next
 
 
 router.get("/:id", catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate("reviews");
+    const campground = await Campground.findById(req.params.id).populate("reviews").populate("author");
     if (!campground) {
         req.flash("error", "Campground not found!");
         return res.redirect("/campgrounds");
