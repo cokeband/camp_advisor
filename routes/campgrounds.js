853c4c703c674ed24720665935a72cf5aa1980d0
Xiaoling Huang <linccccoln@gmail.com>
2022-11-08 11:53:54 -0800

    Added Multer Middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const campgrounds = require("../controllers/campgrounds");
 const { isLoggedIn, isAuthor, validateCampgound } = require("../middleware");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const Campground = require("../models/campground");
 
 router.route("/")
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampgound, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampgound, catchAsync(campgrounds.createCampground));
+    .post(upload.array("image"), (req, res) => {
+        console.log(req.body, req.file);
+        res.send("It worked!")
+    })
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
