da4aee10b90a710af190df462d18ccfce124d172
Xiaoling Huang <linccccoln@gmail.com>
2022-11-09 14:17:10 -0800

    Added request.file

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -8,7 +8,7 @@ cloudinary.config({
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
+    cloudinary,
     params: {
         folder: "CampAdvisor",
         allowedFormats: ["jpg", "png", "jpeg"]
