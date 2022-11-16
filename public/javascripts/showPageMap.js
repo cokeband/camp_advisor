dde0a0ea479a0ef0fe93ed78c7758a943ba39f83
Xiaoling Huang <linccccoln@gmail.com>
2022-11-16 13:48:52 -0800

    Added map pin

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,12 +1,13 @@
 
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-74.5, 40],
-    zoom: 9,
-    projection: 'globe'
+    center: campground.geometry.coordinates,
+    zoom: 10,
 });
-map.on('style.load', () => {
-    map.setFog({});
-});
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
