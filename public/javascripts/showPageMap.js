22afd4e5db6fb3fc0ac4b2b0438aff1266ead43c
Xiaoling Huang <linccccoln@gmail.com>
2022-11-16 14:23:10 -0800

    Customized map popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,11 +3,17 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/streets-v8',
     center: campground.geometry.coordinates,
     zoom: 10,
 });
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
-    .addTo(map);
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h6>${campground.title}</h6><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map)
