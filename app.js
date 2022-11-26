4bf79d3d688bc8b6ab22b99582aafc2b4dd7c853
Xiaoling Huang <linccccoln@gmail.com>
2022-11-25 22:42:58 -0800

    Set up Mongo Atlas

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ const helmet = require("helmet");
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
+const dbUrl = process.env.DB_URL; //mongodb://localhost:27017/campAdvisor
 
+//mongoose.connect(dbUrl);
 mongoose.connect("mongodb://localhost:27017/campAdvisor");
 
 const db = mongoose.connection;
