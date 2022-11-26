f0bf782201982a5e7190f26af16774db4d25547f
Xiaoling Huang <linccccoln@gmail.com>
2022-11-25 23:55:11 -0800

    configure for mongo session store

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ const helmet = require("helmet");
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
-const dbUrl = process.env.DB_URL; //mongodb://localhost:27017/campAdvisor
+const MongoStore = require("connect-mongo");
+//const dbUrl = process.env.DB_URL;
+const dbUrl = "mongodb://localhost:27017/campAdvisor"
+mongoose.connect(dbUrl);
 
-//mongoose.connect(dbUrl);
-mongoose.connect("mongodb://localhost:27017/campAdvisor");
-
-const db = mongoose.connection;
+const db = mongoose.connection
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
@@ -44,9 +44,21 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }))
 
-const sessionConfig = {
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: "thisshouldbeabettersecret!",
+    }
+});
 
-    secret: 'thisshouldbeabettersecret!',
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
+const sessionConfig = {
+    store,
+    secret: "thisshouldbeabettersecret!",
     resave: false,
     saveUninitialized: true,
     cookie: {
